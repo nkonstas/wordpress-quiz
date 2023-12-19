1b441686c09f1492a15aa99c2db22e893429e765
Nikos Konstas <nikos.konstas@gmail.com>
2023-12-19 14:06:29 +0000

    Added build script to create a zip archive that Wordpress can use to install our plugin

---
+++ build-release.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Check if version is provided
+if [ "$#" -ne 1 ]; then
+    echo "Error: Version number is required."
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+# Assign the version from the command line argument
+version="$1"
+
+# Define the list of files and folders to include
+include_list=("LICENSE" "kd-quiz.php", "includes", "assets")
+
+# Define the top-level folder name inside the zip
+top_level_folder_name="kd-quiz"
+
+# Temp directory path
+temp_dir="/tmp/${top_level_folder_name}"
+
+# Get the script's directory
+script_dir="$(dirname "$0")"
+
+# Output directory for the zip file (inside the script's directory)
+output_dir="${script_dir}/output"
+
+# Lock file location
+lock_file="/tmp/${top_level_folder_name}.lock"
+
+# Check for lock file
+if [ -e "$lock_file" ]; then
+    echo "Script is already running."
+    exit 1
+fi
+
+# Create lock file
+touch "$lock_file"
+
+# Ensure removal of the existing temp directory, if it exists
+rm -rf "$temp_dir"
+
+# Create the temp directory
+mkdir -p "$temp_dir"
+
+# Create the output directory
+mkdir -p "$output_dir"
+
+# Copy the files and folders to the temp directory
+for item in "${include_list[@]}"; do
+    cp -r "$item" "$temp_dir/"
+done
+
+# Create the ZIP file with the version suffix
+zip -r "$output_dir/${top_level_folder_name}-${version}.zip" "$temp_dir"
+
+# Clean up: Remove the temporary directory and lock file
+rm -rf "$temp_dir"
+rm -f "$lock_file"
+
+echo "ZIP archive created in $output_dir/${top_level_folder_name}-${version}.zip"
