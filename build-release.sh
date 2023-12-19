4556fb59ad87ede24ec56aacbba99578151949d5
Nikos Konstas <nikos.konstas@gmail.com>
2023-12-19 14:13:44 +0000

    Added basic build script to create a release and a gitignore file to ignore the release folder

--- build-release.sh
+++ build-release.sh
@@ -11,7 +11,7 @@ fi
 version="$1"
 
 # Define the list of files and folders to include
-include_list=("LICENSE" "kd-quiz.php", "includes", "assets")
+include_list=("LICENSE" "kd-quiz.php" "includes" "assets")
 
 # Define the top-level folder name inside the zip
 top_level_folder_name="kd-quiz"
@@ -23,7 +23,7 @@ temp_dir="/tmp/${top_level_folder_name}"
 script_dir="$(dirname "$0")"
 
 # Output directory for the zip file (inside the script's directory)
-output_dir="${script_dir}/output"
+output_dir="${script_dir}/releases"
 
 # Lock file location
 lock_file="/tmp/${top_level_folder_name}.lock"
@@ -51,8 +51,11 @@ for item in "${include_list[@]}"; do
     cp -r "$item" "$temp_dir/"
 done
 
-# Create the ZIP file with the version suffix
-zip -r "$output_dir/${top_level_folder_name}-${version}.zip" "$temp_dir"
+# Remove existing archives
+rm -f "${output_dir}/${top_level_folder_name}-${version}.zip"
+
+# Create the ZIP file with the version suffix and exclude hidden files/folders
+zip -r "${output_dir}/${top_level_folder_name}-${version}.zip" "$temp_dir" -x "*/.*" -x ".*"
 
 # Clean up: Remove the temporary directory and lock file
 rm -rf "$temp_dir"
